25fa86c7b5f6aa1a9c62e3ae7c07c327e9e43786
Sheikh Nizamuddin <sknizm007@gmail.com>
2025-07-31 19:04:03 +0530

    first commit 01

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,6 +1,8 @@
-import { Schema } from "mongoose";
+import { model, models, Schema } from "mongoose";
 
 const CategorySchema = new Schema({
     name:{type:String,required:true},
     store:{type:Schema.Types.ObjectId,ref:"Category",required:true}
 })
+
+export default models.Category || model("Category", CategorySchema)
