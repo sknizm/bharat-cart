acd5d325ca4502a4b57af9f12317c60b6ca7b048
Sheikh Nizamuddin <sknizm007@gmail.com>
2025-08-15 19:14:17 +0530

    fixed product _id issue for vercel 06

--- src/app/[slug]/dashboard/products/add-new/[_id]/page.tsx
+++ src/app/[slug]/dashboard/products/add-new/[_id]/page.tsx
@@ -1,5 +1,5 @@
 import AddUpdateProduct from "../AddUpdateProduct";
-
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function UpdateProduct({ params }: any) {
   const { _id } = params;
   return <AddUpdateProduct _id={_id} />;
