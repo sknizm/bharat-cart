cca4c9e564b27e30f424771c3175c1a51f049d3a
Sheikh Nizamuddin <sknizm007@gmail.com>
2025-08-16 10:41:28 +0530

    fixed product _id issue for vercel 11

--- src/app/api/store/[slug]/product/[_id]/route.ts
+++ src/app/api/store/[slug]/product/[_id]/route.ts
@@ -4,11 +4,10 @@ import Product from "@/models/Product";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    params: any
+    { params }: { params: { slug: string; _id: string } }
 ) {
     try {
-        const {_id} =  params;
+        const { _id } = params;
 
         await connectDB();
         const product = await Product.findById(_id).lean().exec();
@@ -25,30 +24,29 @@ export async function GET(req: Request,
 }
 
 export async function PUT(req: Request,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    params: any
+    { params }: { params: { slug: string; _id: string } }
 ) {
-   try{
-     const user = await getCurrentUser();
-    if (!user) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    try {
+        const user = await getCurrentUser();
+        if (!user) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
 
-    const { _id } = params;
-    const { name, description, price, salePrice, categories, images } = await req.json();
+        const { _id } = params;
+        const { name, description, price, salePrice, categories, images } = await req.json();
 
-    const product = await Product.findByIdAndUpdate({ _id }, {
-        name: name,
-        description: description,
-        images: images,
-        price: price,
-        salePrice: salePrice,
-        categories: categories
-    },{new:true});
+        const product = await Product.findByIdAndUpdate({ _id }, {
+            name: name,
+            description: description,
+            images: images,
+            price: price,
+            salePrice: salePrice,
+            categories: categories
+        }, { new: true });
 
-    if(!product) return NextResponse.json({error:"Product not found"},{status:404});
-    return NextResponse.json({success:true});
-   }catch(error){
-    console.error(error);
-    return NextResponse.json({error:"Internal server error"},{status:500});
-   }
+        if (!product) return NextResponse.json({ error: "Product not found" }, { status: 404 });
+        return NextResponse.json({ success: true });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+    }
 
 }
