bc86eb0d0db4c549d48b52fe68d72fde894bfb2b
Sheikh Nizamuddin <sknizm007@gmail.com>
2025-08-25 21:29:15 +0530

    fixed package json for vercel 13

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,3 +1,8 @@
-const config = 
-{ plugins: ["@tailwindcss/postcss"], };
- export default config;
+const config = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
+export default config;
